9955aa787984e46fc06375d4b7ddecf9ec3dfc1b
ra3738 <rithvik.al@gmail.com>
2021-04-29 12:51:16 -0700

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import tileData from './tileData';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    padding: '50px',
+    marginRight: '50px'
+  },
+  gridList: {
+    width: 600,
+  },
+  search: {
+    flex: '1',
+    paddingLeft: '50px',
+    paddingRight: '100px'
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <div className={classes.search}>
+        <h1>Image Repo</h1>
+        <br/><br/><br/><br/><br/>
+        <TextField fullWidth="true" id="outlined-basic" label="Search" variant="outlined" />
+      </div>
+      <GridList cellHeight={180} className={classes.gridList} cols={3}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img} cols={tile.cols || 1}>
+            <img src={tile.img} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
 
-export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/tileData.js
@@ -0,0 +1,69 @@
+const tileData = [
+  {
+    img: '/images/000000000285.jpg',
+    title: 'Bear',
+    author: 'jill111',
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: '/images/000000000785.jpg',
+    title: 'Ski',
+    author: 'director90',
+  },
+  {
+    img: '/images/000000001584.jpg',
+    title: 'Bus',
+    author: 'Danson67',
+  },
+  {
+    img: '/images/000000005037.jpg',
+    title: 'Bus',
+    author: 'fancycrave1',
+    featured: true,
+  },
+  {
+    img: '/images/000000007816.jpg',
+    title: 'Bike',
+    author: 'Hans',
+  },
+  {
+    img: '/images/000000007888.jpg',
+    title: 'Clock',
+    author: 'fancycravel',
+  },
+  {
+    img: '/images/000000013597.jpg',
+    title: 'Cake',
+    author: 'jill111',
+    cols: 2,
+  },
+  {
+    img: '/images/000000019432.jpg',
+    title: 'Tennis',
+    author: 'BkrmadtyaKarki',
+  },
+  {
+    img: '/images/000000027768.jpg',
+    title: 'Bus',
+    author: '../publicDomainPictures',
+  },
+  {
+    img: '/images/000000028449.jpg',
+    title: 'Elephant',
+    author: 'congerdesign',
+  },
+  {
+    img: '/images/000000032081.jpg',
+    title: 'Tennis',
+    cols: 2,
+    author: '821292',
+  },
+  {
+    img: '/images/000000033368.jpg',
+    title: 'Tennis',
+    author: 'danfador',
+  },
+];
+
+export default tileData;
